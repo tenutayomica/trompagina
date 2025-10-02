6344da2fd601f81de5f2d94a787f283eb4b91c3c
tenutayomica <micatenu@gmail.com>
2025-10-02 20:59:10 -0300

    begin tromping

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import pag1 from './components/pag1';
+function App() {
+  return (
+    <div className="App">
+      <pag1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pag1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import navbar from './navbar';
+const pag1 = () => {
+  return (
+    <div className='pag1-container'>
+        <navbar/>
+    </div>
+  )
+}
+
+export default pag1
